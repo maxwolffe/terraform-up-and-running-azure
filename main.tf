abe5148aca39b34fa5bcdb4f134475b0340d4fe0
Max Wolffe <mwolffe@mwolffe-mn4.linkedin.biz>
2019-12-15 11:56:23 -0800

    Add working public ip and security group rules

--- main.tf
+++ main.tf
@@ -29,15 +29,42 @@ resource "azurerm_subnet" "internal" {
   address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-public-ip"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-security-group"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "main" {
+  name                      = "${var.prefix}-nic"
+  location                  = "${azurerm_resource_group.main.location}"
+  resource_group_name       = "${azurerm_resource_group.main.name}"
+  network_security_group_id = "${azurerm_network_security_group.main.id}"
 
   ip_configuration {
     name                          = "testconfiguration1"
     subnet_id                     = "${azurerm_subnet.internal.id}"
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.main.id}"
   }
 }
 resource "azurerm_virtual_machine" "main" {
@@ -69,8 +96,17 @@ resource "azurerm_virtual_machine" "main" {
     computer_name  = "hostname"
     admin_username = "mwolffe"
     admin_password = "SecretPassword!"
+    custom_data    = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   }
   os_profile_linux_config {
     disable_password_authentication = false
   }
+}
+
+output "public-ip" {
+  value = "${azurerm_public_ip.main.ip_address}"
 }
